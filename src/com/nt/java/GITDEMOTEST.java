0d4d6476093f177aa1d9ad5e9a530591b5b65de6
HP <sraut4849@gmail.com>
2023-11-30 14:25:45 +0530

    ABSI7456: this is my First Commit

---
+++ src/com/nt/java/GITDEMOTEST.java
@@ -0,0 +1,13 @@
+package com.nt.java;
+
+public class GITDEMOTEST {
+	int a=10;
+	int b=20;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("GITDEMO CLASS");
+		
+	}
+
+}
