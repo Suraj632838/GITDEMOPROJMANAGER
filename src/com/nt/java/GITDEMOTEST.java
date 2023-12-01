a99a0d0ebce88429fe8447b7bad3d1fc62561c47
HP <sraut4849@gmail.com>
2023-12-01 14:50:23 +0530

    asa

--- src/com/nt/java/GITDEMOTEST.java
+++ src/com/nt/java/GITDEMOTEST.java
@@ -7,7 +7,7 @@ public class GITDEMOTEST {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("GITDEMO CLASS");
-    System.out.println("CANTCONNCE");
+    System.out.println("CANTCONNCE  ");
 		
 	}
 
